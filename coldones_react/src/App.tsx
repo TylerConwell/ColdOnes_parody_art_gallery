6fcf653706da341e7781205119f575b368fd38ba
Tyler Conwell <wgc14@txstate.edu>
2025-12-03 16:31:16 -0600

    starting the project with a button and text

--- coldones_react/src/App.tsx
+++ coldones_react/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,25 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      {/* working title for the gallery */}
+      <h1>Cold Ones Art Gallery</h1>
+
+      {/* Stim button for randomly pressing */}
+      <div className="fun_button">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Wow you clicked this button {count} times
         </button>
+
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Incase you needed a way to relieve stress, 
+          or use it as a voting mechanism since I havent made one yet.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        
+      </div>
+
     </>
   )
 }
